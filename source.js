439e3548868207eede29a815641211b2f9f4456f
Marcos Dassan <46528720+marcosadassan@users.noreply.github.com>
2025-03-31 15:50:17 -0300

    Add files via upload

--- source.js
+++ source.js
@@ -16,35 +16,20 @@ function createSquaresFromPoints(points, sideLength) {
       corner2.coordinates(),
       corner3.coordinates(),
       corner4.coordinates(),
-      corner1.coordinates() // Close the polygon
+      corner1.coordinates() 
     ]);
 
     return ee.Feature(square);
   });
 }
 
-function exportImageByFeatureSeco(feature) {
-  var geometry = feature.geometry();
-  var croppedImage = imageSeco.clip(geometry);
-
-  Export.image.toDrive({
-    image: croppedImage,
-    region: geometry,
-    scale: 10,
-    maxPixels: 1e13
-  });
-}
-
-
 function maskS2clouds(image) {
+	
   var qa = image.select('QA60');
-
-  // Bits 10 e 11 são nuvens e bits 12 é cirrus.
   var cloudBitMask = 1 << 10;
   var cirrusBitMask = 1 << 11;
   var cloudShadowBitMask = 1 << 12;
 
-  // Ambos os bits devem ser zero, indicando condições claras.
   var mask = qa.bitwiseAnd(cloudBitMask).eq(0)
       .and(qa.bitwiseAnd(cirrusBitMask).eq(0))
       .and(qa.bitwiseAnd(cloudShadowBitMask).eq(0));
@@ -56,11 +41,7 @@ function maskS2clouds(image) {
 var squareSideLength = 0.1;
 var squares = createSquaresFromPoints(geometry, squareSideLength);
 
-var imageSeco = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
-  .filterBounds(squares)
-  .filterDate('2024-04-01', '2024-09-30').map(maskS2clouds).median();
-
-var imageUmido = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
+var image = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
   .filterBounds(squares)
   .filterDate('2023-10-01', '2024-03-31').map(maskS2clouds).median();
 
@@ -72,11 +53,11 @@ ee.List.sequence(0, featureCount.subtract(1)).evaluate(function(indices) {
   indices.forEach(function(index) {
     var feature = ee.Feature(featureList.get(index));
     var geometry = feature.geometry();
-    var croppedImage = imageUmido.clip(geometry);
+    var croppedImage = image.clip(geometry);
 
     Export.image.toDrive({
       image: croppedImage,
-      folder: 'cafe',
+      folder: 'imagens',
       region: geometry,
       scale: 10,
       maxPixels: 1e13
